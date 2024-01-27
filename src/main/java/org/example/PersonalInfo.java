757042d132464f634398a8a43d1e19f18dc9fd4e
satheesh.ceiyone@gmail.com <satheesh.ceiyone@gmail.com>
2024-01-27 13:48:56 +0530

    initial commit

---
+++ src/main/java/org/example/PersonalInfo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class PersonalInfo {
+
+
+    public static void main (String args[]){
+        System.out.println("My Personal Information");
+        System.out.println("Name:JAYASURYAN S");
+        System.out.println("Age : 20");
+        System.out.println("Birthday :July 15,2003");
+        System.out.println("Department:Biotechnology");
+        System.out.println("College:VISTAS");
+        System.out.println("Contact:8248323975");
+    }
+
+}
