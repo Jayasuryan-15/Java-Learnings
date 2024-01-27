77642abc9d941031f575dcebc490aa96bd68cc42
satheesh.ceiyone@gmail.com <satheesh.ceiyone@gmail.com>
2024-01-27 19:19:12 +0530

    adding file

---
+++ src/main/java/org/example/MyMarks.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MyMarks {
+    public static void main (String[] args){
+        System.out.println("Semester wise mark Details;");
+        System.out.println("First Semester GPA : 9.1");
+        System.out.println("Second Semester GPA:9.2");
+        System.out.println("Third Semester GPA :7.6");
+        System.out.println("Fourth Semester GPA:7.8");
+        System.out.println("Fifth Semester GPA:8.6");
+        System.out.println("Sixth Semester GPA:8.2");
+        System.out.println("Average of total Semester CGPA:8.4");
+    }
+}
