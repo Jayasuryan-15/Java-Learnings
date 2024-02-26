b09662a4cdd572f385283d265fc194133317b502
satheesh.ceiyone@gmail.com <satheesh.ceiyone@gmail.com>
2024-02-26 18:32:16 +0530

    Linkedlist example

---
+++ src/main/java/com/javalearnings/Collection/TrainLinkedList.java
@@ -0,0 +1,26 @@
+package com.javalearnings.Collection;
+
+import java.util.LinkedList;
+
+public class TrainLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> train = new LinkedList<>();
+        train.add("Alice");
+        train.add("Bob");
+        train.add("Charlie");
+
+
+        train.add(1, "David");
+        train.add(4, "Tim");
+
+        train.remove("Bob");
+
+
+        System.out.println("Passengers on board: " + train);
+
+
+        System.out.println("Front passenger: " + train.getFirst());
+        System.out.println("Last passenger: " + train.getLast());
+    }
+
+}
