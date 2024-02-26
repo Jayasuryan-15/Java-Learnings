1a54b0723f4413ce12155375daec4f4225878dec
satheesh.ceiyone@gmail.com <satheesh.ceiyone@gmail.com>
2024-02-26 19:02:57 +0530

    PriorityQueue example

---
+++ src/main/java/com/javalearnings/Collection/main.java
@@ -0,0 +1,28 @@
+package com.javalearnings.Collection;
+
+import java.util.PriorityQueue;
+
+public class main {
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+
+        pq.add(10);
+        pq.add(20);
+        pq.add(15);
+        pq.add(5);
+
+         System.out.println("PriorityQueue: " + pq);
+        //System.out.println("PriorityQueue before removal: " + pq);
+       // pq.remove(15);
+
+
+       while (!pq.isEmpty()) {
+            System.out.println("Removed: " + pq.poll());
+          //  System.out.println("PriorityQueue After removal: " + pq);
+        }
+    }
+}
+
+
