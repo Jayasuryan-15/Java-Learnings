2ec3bebb1074f312fecabf773e555600d6097e06
satheesh.ceiyone@gmail.com <satheesh.ceiyone@gmail.com>
2024-03-15 14:42:40 +0530

    Problems

---
+++ src/main/java/Problems/MultithreadingExample.java
@@ -0,0 +1,31 @@
+package Problems;
+
+public class MultithreadingExample {
+    public static void main(String[] args) {
+        Thread thread1 = new MyThread("Thread 1");
+        Thread thread2 = new MyThread("Thread 2");
+
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class MyThread extends Thread {
+    private String name;
+
+    public MyThread(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(name + ": " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
