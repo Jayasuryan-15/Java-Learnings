cbca9d0813a3c6fa70f3ed849e629f52c93d7610
satheesh.ceiyone@gmail.com <satheesh.ceiyone@gmail.com>
2024-04-23 19:52:47 +0530

    Problems

---
+++ src/main/java/Problems1/java/SumOfDigits.java
@@ -0,0 +1,27 @@
+package Problems1.java;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Input: " + num);
+        System.out.println("Output: " + sumOfDigits(num));
+    }
+
+    public static int sumOfDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
